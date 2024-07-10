c269bc497c9e13c8b742d3f7830853147fdc6f17
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-10 14:08:39 -0400

    added links to the APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      
+        <Routes>
+            <Route path="/" element={} />
+            <Route path="/companies" element={} />
+            <Route path="/companies/:company" element={} />
+            <Route path="/jobs" element={} />
+            <Route path="/login" element={} />
+            <Route path="/signup" element={} />
+            <Route path="/profile" element={} />
+        </Routes>
     </div>
   );
 }
