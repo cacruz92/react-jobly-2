3237ba577b3a11b2d2bf45e50e2101e7c63dbc8e
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-10 22:27:06 -0400

    set up the app with useEffect to do an API request to pull companies and jobs. added those to state to pass down to the components and links. allowed to Companies component to render a list of links with the company names, using that data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes} from "react-router-dom";
+import JoblyApi from "./api";
 import Home from "./Home";
 import Companies from './Companies';
 import Company from './Company';
@@ -7,13 +8,44 @@ import Jobs from './Jobs';
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
 import Profile from './Profile';
+import { useEffect, useState } from 'react';
+import NavBar from './NavBar';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [companyList, setCompanyList] = useState([]);
+  const [jobList, setJobList] = useState([])
+
+  useEffect(() => {
+    async function getCompanies() {
+      
+      let companies = await JoblyApi.request('companies');
+      console.log("Fetched companies:", companies); // Debug log
+      setCompanyList(companies.companies);
+      setIsLoading(false);
+    }
+    getCompanies();
+  }, [])
+
+  useEffect(() => {
+    async function getJobs() {
+      let jobs = await JoblyApi.request('jobs');
+      setJobList(jobs);
+      setIsLoading(false);
+    }
+    getJobs();
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
+  }
+
   return (
     <div className="App">
+      <NavBar />
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/companies" element={<Companies />} />
+            <Route path="/companies" element={<Companies companyList={companyList} title="Companies" />} />
             <Route path="/companies/:company" element={<Company />} />
             <Route path="/jobs" element={<Jobs />} />
             <Route path="/login" element={<LoginForm />} />
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import NavBar from "./NavBar";
+
 
 const Home = () => {
     return(
         <>
-        <NavBar />
         <h1> Jobly </h1>
         <h3> All the jobs in one, convenient place. </h3>
         <h2> Welcome Back! </h2>
