ab418c74b363ae7facff50ace6653f1b87b77b61
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-24 17:58:19 -0400

    final changes

---
+++ frontend/src/Jobs.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    CardText,
+    ListGroup,
+    ListGroupItem
+  } from "reactstrap";
+import SearchForm from "./SearchForm";
+
+const Jobs = ({jobList}) => {
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default Jobs;
